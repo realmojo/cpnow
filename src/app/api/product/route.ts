9d95c9e2984e67231dd083da46de23edb499005f
realmojo <strikers1999@naver.com>
2025-04-20 12:04:15 +0900

    ff

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -7,7 +7,7 @@ type ProductParam = {
   itemId: string;
 };
 
-const getCoupangItemRequest = (params: ProductParam) => {
+const getCoupangItemRequest = async (params: ProductParam) => {
   return new Promise((resolve, reject) => {
     const myHeaders = new Headers();
     const { productId, vendorItemId, itemId } = params;
@@ -15,10 +15,19 @@ const getCoupangItemRequest = (params: ProductParam) => {
       "Referer",
       `https://www.coupang.com/vp/products/${productId}?itemId=${vendorItemId}&vendorItemId=${itemId}`,
     );
-    myHeaders.append(
-      "Cookie",
-      "bm_s=YAAQDNojF8mAiEiWAQAAv47rSAPzX9vBQgD46eyg3ZZBP297KPcVCjsZjNdX01Nc8WjoBkU4HSIla1U9xtiM2VzeM6VBHl7p2gnXX25sTvRrkBgQnDeQz8pfq4gWuLNf+tB4V75H8ws4/WCJhTY1oDsU++9xjMy7rxiy7N8QfExz3jRcAter6BkXbpq9qTmsOAkI7bZ9D1eC0hTBoTrLBPbakJMvzqjZnAELA9QIzP6jPjCIdNFqcwmnZ2By/J/x9efMaX76w+hmkS2YVopBmhoKN9tE9Vmg02svD1F1OQJB+GsxBmcRWgxjbXHlqhkqCluortVKZLRDXUm0WRxbTnF7eY2epPHzZeX3GSYundH8bLk0BBY2hu9y0D5xSDiboiBEvLrBdJ4SYbLolFlYRTzU/i7xZkaM/HN6PHRHYbGhfaJPIvLijmiLzqTQp6Ht1gxN69iC0+euVQ==;",
-    );
+    // myHeaders.append(
+    //   "User-Agent",
+    //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    // );
+    // myHeaders.append(
+    //   "Cookie",
+    //   "bm_s=YAAQDNojF8mAiEiWAQAAv47rSAPzX9vBQgD46eyg3ZZBP297KPcVCjsZjNdX01Nc8WjoBkU4HSIla1U9xtiM2VzeM6VBHl7p2gnXX25sTvRrkBgQnDeQz8pfq4gWuLNf+tB4V75H8ws4/WCJhTY1oDsU++9xjMy7rxiy7N8QfExz3jRcAter6BkXbpq9qTmsOAkI7bZ9D1eC0hTBoTrLBPbakJMvzqjZnAELA9QIzP6jPjCIdNFqcwmnZ2By/J/x9efMaX76w+hmkS2YVopBmhoKN9tE9Vmg02svD1F1OQJB+GsxBmcRWgxjbXHlqhkqCluortVKZLRDXUm0WRxbTnF7eY2epPHzZeX3GSYundH8bLk0BBY2hu9y0D5xSDiboiBEvLrBdJ4SYbLolFlYRTzU/i7xZkaM/HN6PHRHYbGhfaJPIvLijmiLzqTQp6Ht1gxN69iC0+euVQ==;",
+    // );
+    myHeaders.append("Accept", "*/*");
+    myHeaders.append("Accept-Encoding", "gzip, deflate, br, zstd");
+    myHeaders.append("Accept-Language", "ko-KR,ko;q=0.9");
+
+    console.log(myHeaders);
 
     const requestOptions: RequestInit = {
       method: "GET",
@@ -27,12 +36,40 @@ const getCoupangItemRequest = (params: ProductParam) => {
     };
 
     const url = `https://www.coupang.com/vp/products/${productId}/vendoritems/${vendorItemId}/quantity-info?quantity=1`;
+    console.log(requestOptions);
 
     fetch(url, requestOptions)
       .then((response) => response.json())
       .then((result) => resolve(result))
-      .catch(() => reject("error"));
+      .catch(() =>
+        reject(
+          `🚨 쿠팡 수량 조회 실패: productId(${productId}), vendorItemId(${vendorItemId}), itemId(${itemId})`,
+        ),
+      );
   });
+
+  // const { productId, vendorItemId, itemId } = params;
+
+  // const url = `https://www.coupang.com/vp/products/${productId}/vendoritems/${vendorItemId}/quantity-info?quantity=1`;
+  // console.log(123);
+  // try {
+  //   console.log(url);
+  //   const response = await axios.get(url, {
+  //     headers: {
+  //       Referer: `https://www.coupang.com/vp/products/${productId}?itemId=${vendorItemId}&vendorItemId=${itemId}`,
+  //       "User-Agent":
+  //         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+  //       Cookie:
+  //         "bm_s=YAAQDNojF8mAiEiWAQAAv47rSAPzX9vBQgD46eyg3ZZBP297KPcVCjsZjNdX01Nc8WjoBkU4HSIla1U9xtiM2VzeM6VBHl7p2gnXX25sTvRrkBgQnDeQz8pfq4gWuLNf+tB4V75H8ws4/WCJhTY1oDsU++9xjMy7rxiy7N8QfExz3jRcAter6BkXbpq9qTmsOAkI7bZ9D1eC0hTBoTrLBPbakJMvzqjZnAELA9QIzP6jPjCIdNFqcwmnZ2By/J/x9efMaX76w+hmkS2YVopBmhoKN9tE9Vmg02svD1F1OQJB+GsxBmcRWgxjbXHlqhkqCluortVKZLRDXUm0WRxbTnF7eY2epPHzZeX3GSYundH8bLk0BBY2hu9y0D5xSDiboiBEvLrBdJ4SYbLolFlYRTzU/i7xZkaM/HN6PHRHYbGhfaJPIvLijmiLzqTQp6Ht1gxN69iC0+euVQ==;",
+  //     },
+  //     // maxRedirects: 5, // redirect: 'follow' 대체
+  //   });
+
+  //   return response.data;
+  // } catch (error) {
+  //   console.error("🚨 쿠팡 수량 조회 실패:", error);
+  //   throw new Error("Coupang quantity info fetch failed");
+  // }
 };
 
 export async function GET(req: NextRequest) {
@@ -49,26 +86,10 @@ export async function GET(req: NextRequest) {
     }
 
     // ✅ 외부 API 호출
-    // const apiRes = await fetch(
-    //   `https://api.mindpang.com/api/coupang/getItemById.php?id=${id}`,
-    // );
-
-    // if (!apiRes.ok) {
-    //   return new Response(
-    //     JSON.stringify({ error: "Failed to fetch from external API" }),
-    //     {
-    //       status: 502,
-    //       headers: { "Content-Type": "application/json" },
-    //     },
-    //   );
-    // }
-    console.log("id", id);
     const { data } = await axios.get(
       `https://api.mindpang.com/api/coupang/getItemById.php?id=${id}`,
     );
-    console.log(data);
 
-    // const data = await apiRes.json();
     const { productId, vendorItemId, itemId, lastUpdated, price } = data;
 
     data.thumbnail = data.thumbnail.replace("230x230", "600x600");
@@ -81,7 +102,6 @@ export async function GET(req: NextRequest) {
     });
 
     // 새롭게 가격을 가져옴
-    console.log(2, lastUpdated, getTodayDate());
     if (!lastUpdated || getTodayDate() !== lastUpdated.substring(0, 10)) {
       const nowPrice = coupangItem[0].moduleData[3].priceInfo.finalPrice.price
         ? coupangItem[0].moduleData[3].priceInfo.finalPrice.price
