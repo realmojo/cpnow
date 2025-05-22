5129093e8c6c19fb6bd691c41c9e89f01311a014
realmojo <strikers1999@naver.com>
2025-05-22 17:13:27 +0900

    헤ㅐ애

--- src/app/register-service-worker.tsx
+++ src/app/register-service-worker.tsx
@@ -1,52 +1,44 @@
 "use client";
 
 import { useEffect } from "react";
+import { Workbox } from "workbox-window"; // ✅ npm 설치된 모듈 import
 
 export default function RegisterServiceWorker() {
   useEffect(() => {
     if (typeof window === "undefined" || !("serviceWorker" in navigator))
       return;
 
-    const loadWorkbox = async () => {
-      // 👇 TypeScript에게 이 줄의 타입 오류를 무시하라고 지시
-      // @ts-ignore
-      const module = await import(
-        "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-window.prod.mjs"
-      );
+    const wb = new Workbox("/firebase-messaging-sw.js?v=1.0.13");
 
-      const wb = new module.Workbox("/firebase-messaging-sw.js?v=1.0.13");
-
-      const forceUpdateSW = async () => {
-        wb.addEventListener("controlling", () => {
-          console.log(
-            "✅ New service worker is now controlling the page. Reloading...",
-          );
-          window.location.reload();
-        });
-
-        wb.messageSkipWaiting();
-      };
-
-      wb.addEventListener("waiting", () => {
+    const forceUpdateSW = async () => {
+      wb.addEventListener("controlling", () => {
         console.log(
-          "🆕 New Service Worker waiting to activate. Forcing activation...",
+          "✅ New service worker is now controlling the page. Reloading...",
         );
-        forceUpdateSW();
+        window.location.reload();
       });
 
-      wb.addEventListener("activated", () => {
-        console.log("🔄 Service Worker activated");
-      });
-
-      try {
-        const reg = await wb.register();
-        console.log("🔐 Service Worker registered", reg);
-      } catch (err) {
-        console.error("❌ Service Worker registration failed:", err);
-      }
+      wb.messageSkipWaiting();
     };
 
-    loadWorkbox();
+    wb.addEventListener("waiting", () => {
+      console.log(
+        "🆕 New Service Worker waiting to activate. Forcing activation...",
+      );
+      forceUpdateSW();
+    });
+
+    wb.addEventListener("activated", () => {
+      console.log("🔄 Service Worker activated");
+    });
+
+    wb.register()
+      .then((reg) => {
+        console.log("🔐 Service Worker registered", reg);
+      })
+      .catch((err) => {
+        console.error("❌ Service Worker registration failed:", err);
+      });
   }, []);
 
   return null;
