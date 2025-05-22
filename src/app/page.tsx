7038dfd249ff06404cc27b149431aa3a3c078d69
realmojo <strikers1999@naver.com>
2025-05-22 11:33:55 +0900

    p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,6 @@ import RecentlyDiscountedProducts from "@/src/components/home/RecentlyDiscounted
 export default async function Home() {
   return (
     <main className="mx-auto w-full max-w-[800px] space-y-10 px-4 py-6">
-      <section className="text-center">
-        <h1 className="text-2xl leading-snug font-bold sm:text-3xl">
-          지금 이 순간 <br className="block sm:hidden" />
-          가장 똑똑하게 소비하는 방법
-        </h1>
-      </section>
       <RecentlyDiscountedProducts />
       <CategoryProductSection fisrtCategories={fisrtCategories} />
     </main>
--- src/components/home/RecentlyDiscountedProducts.tsx
+++ src/components/home/RecentlyDiscountedProducts.tsx
@@ -16,9 +16,6 @@ export default function RecentlyDiscountedProducts() {
 
   return (
     <div className="mt-6">
-      <h2 className="scroll-m-20 text-2xl font-bold tracking-tight">
-        최근 가격이 변동한 상품
-      </h2>
       <ProductList items={products} type="list" />
     </div>
   );
