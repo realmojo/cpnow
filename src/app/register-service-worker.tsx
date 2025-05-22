dcc5d6161bf077b08a0666f0677a4a9cb52cc2d8
realmojo <strikers1999@naver.com>
2025-05-22 16:23:27 +0900

    gg

--- src/app/register-service-worker.tsx
+++ src/app/register-service-worker.tsx
@@ -7,7 +7,28 @@ export default function RegisterServiceWorker() {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("/firebase-messaging-sw.js?v=v1.0.12")
-        .then(() => console.log("✅ Service Worker 등록 완료 v1.0.12"))
+        .then((registration) => {
+          // 강제 업데이트 시도
+          registration.update();
+
+          // 변경 감지
+          registration.onupdatefound = () => {
+            const newWorker = registration.installing;
+            if (newWorker) {
+              newWorker.onstatechange = () => {
+                if (newWorker.state === "installed") {
+                  if (navigator.serviceWorker.controller) {
+                    // 새 서비스워커가 설치되었음
+                    console.log(
+                      "✅ Service Worker 등록 완료 v1.0.12",
+                      registration,
+                    );
+                  }
+                }
+              };
+            }
+          };
+        })
         .catch((err) => console.error("Service Worker 등록 실패:", err));
     }
   }, []);
