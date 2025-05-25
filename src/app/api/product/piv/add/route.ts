166e425bd8be41f1719aff95d16177352267747f
realmojo <strikers1999@naver.com>
2025-05-25 16:40:29 +0900

    gagag

--- src/app/api/product/piv/add/route.ts
+++ src/app/api/product/piv/add/route.ts
@@ -91,43 +91,42 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "Missing parameter" }, { status: 400 });
     }
 
-    const query =
+    let query =
       "SELECT * FROM products p where productId = ? AND itemId = ? AND vendorItemId = ?";
     const products = await queryList(query, [productId, itemId, vendorItemId]);
 
-    console.log(products);
+    const params = {
+      userId: userId,
+      pId: 0,
+    };
+
     const product = products.length > 0 ? products[0] : null;
     if (product === null) {
-      const params = {
+      const productParams = {
         userId: userId,
         productId: productId,
         itemId: itemId,
         vendorItemId: vendorItemId,
       };
 
-      const pId = await addNewProduct(params);
+      const pId = await addNewProduct(productParams);
 
-      const alarmParams = {
-        userId: userId,
-        pId: pId,
-      };
-
-      await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/alarm`, {
-        method: "POST",
-        body: JSON.stringify(alarmParams),
-      });
+      params.pId = pId;
     } else if (product.id && userId) {
-      const params = {
-        userId: userId,
-        pId: product.id,
-      };
-
-      await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/alarm`, {
-        method: "POST",
-        body: JSON.stringify(params),
-      });
+      params.pId = product.id;
     } else {
       throw new Error("상품 등록 중 오류가 났습니다");
+    }
+
+    if (params.userId && params.pId) {
+      query =
+        "INSERT IGNORE INTO user_alarms (id, userId, pId, regdated) VALUES (NULL, ?, ?, NOW())";
+      await insertOne(query, [userId, params.pId]);
+
+      // 대기 알람에도 넣어주기
+      query =
+        "INSERT INTO crawl_wait (pId, type, regdated) VALUES (?, 'alarm', NOW()) ON DUPLICATE KEY UPDATE type = 'alarm';";
+      await insertOne(query, [params.pId]);
     }
 
     // ✅ 결과 반환
