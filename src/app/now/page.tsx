5be2d2c84ce21164054fb29ca54a05cd5f0c696a
realmojo <strikers1999@naver.com>
2025-05-26 15:54:46 +0900

    g

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -4,9 +4,13 @@ import RecentlyDiscountedProducts from "@/src/components/home/RecentlyDiscounted
 export default function NowPage() {
   return (
     <article>
-      <section className="mx-auto w-full max-w-[800px] space-y-10 px-4 py-6">
-        <RecentlyDiscountedProducts />
-        {/* <CategoryProductSection fisrtCategories={fisrtCategories} /> */}
+      <section className="mt-4 mb-24 flex justify-center">
+        <div className="w-[800px] px-4">
+          <h2 className="font-heading scroll-m-20 border-none text-2xl font-bold tracking-tight first:mt-0 sm:border-b">
+            지금 가격이 바뀐 상품
+          </h2>
+          <RecentlyDiscountedProducts />
+        </div>
       </section>
     </article>
   );
--- src/components/mynow/Container.tsx
+++ src/components/mynow/Container.tsx
@@ -164,7 +164,7 @@ export default function MyNowContainer() {
 
   return (
     <article>
-      <section className="mt-4 flex justify-center">
+      <section className="mt-4 mb-24 flex justify-center">
         <div className="w-[800px] px-4">
           {myProductsItems.length !== 0 ? (
             // ✅ 데이터 있음
