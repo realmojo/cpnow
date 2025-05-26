fe8b97bb86d57106a578597df381bdcb79a102dd
realmojo <strikers1999@naver.com>
2025-05-26 17:45:49 +0900

    Update Header.tsx

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
-import Logo from "@/public/cpnow-logo.png";
 import MainSidebarRightButton from "../MainSidebarRightButton";
 
 export default function Header() {
