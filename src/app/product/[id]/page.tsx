ca5a8102d8c29e1a286918af9515fadd930cde4e
realmojo <strikers1999@naver.com>
2025-05-30 13:59:16 +0900

    dd

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,5 @@
 import ProductModalPage from "@/src/app/@modal/(.)product/[id]/page";
 
 export default async function ProductPage({ params }: any) {
-  const { id } = await params;
   return <ProductModalPage params={params} />;
 }
