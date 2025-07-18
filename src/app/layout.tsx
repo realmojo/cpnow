6df36d445cbc5f8ac59e32a0ac10b7bca28cf6d3
realmojo <strikers1999@naver.com>
2025-07-18 17:53:48 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,7 +118,7 @@ export default async function RootLayout({
           <Header />
           {children}
           {modal}
-          <Footer />
+          {/* <Footer /> */}
         </main>
         <LayoutClientWrapper />
         <Toaster
