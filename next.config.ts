e17453116579712321cae21116a65a6b33cff43a
정만경/방송DX팀 <6177160+realmojo@users.noreply.github.com>
2025-12-15 17:07:55 +0900

    ㅈㄷㄹ

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["got-scraping", "header-generator"],
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/track": ["./node_modules/header-generator/data_files/**/*"],
-    },
-  },
 };
 
 export default nextConfig;
